d5d7fa1bf52a6bef049fcbb7ec5847f7a18547fa
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-10 19:27:43 -0300

    Time

---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.BackgroundColor = (ConsoleColor)15;Console.ForegroundColor = (ConsoleColor)12;Console.Write("São Pau"); Console.ForegroundColor = (ConsoleColor)0; Console.Write("lo");
